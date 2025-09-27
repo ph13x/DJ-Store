5e845ef71c8bf02c81e2734375365190ace81562
ph13x <ldetermination7@gmail.com>
2025-09-27 05:03:12 +0000

    Prototype v1

--- Home.py
+++ Home.py
@@ -1,23 +1,38 @@
 import streamlit as st
 import pandas as pd
+import fancy_images as fi
+
+st.set_page_config(layout="wide")
 
 data = pd.read_csv("data.csv")
+
 st.title("D.J Darling's")
 
 st.header("Our Items")
 st.write("---")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2  = st.columns([1.5, .5, 1.5])
 
 with col1:
     for index, row in data[:5].iterrows():
         st.subheader(row["name"])
+        st.image(f"images/{row['images']}")
         st.write(row["category"])
         st.subheader(row["price"])
         st.write(row["stock"])
+
+with empty_col:
+    print("""No cost too great," 
+    "No mind to think" 
+    "No will to break" 
+    "No voice to cry suffering" 
+    "Born of God and Void" 
+    "You shall seal the blinding light that plauges their dreams"
+    "You, are the vessel""")
 with col2:
     for index, row in data[5:].iterrows():
         st.subheader(row["name"])
+        st.image(f"images/{row['images']}")
         st.write(row["category"])
         st.subheader(row["price"])
         st.write(row["stock"])
